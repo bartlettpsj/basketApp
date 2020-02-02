5987aba9c6afdb9ddd1663ef719fbf5b6cab0050
Paul Bartlett <paul.bartlett@walmartlabs.com>
2020-02-02 14:32:29 -0800

    Add those missing files

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Basket, DataService} from '../data.service';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss']
+})
+export class ContentComponent implements OnInit {
+  private data: {};
+  private baskets: Basket[];
+
+  constructor(public dataService: DataService) { }
+
+  ngOnInit() {
+    this.baskets = this.dataService.getAllBaskets();
+    console.log('Baskets', this.baskets);
+    console.log('Name:', this.dataService.getBasketNames());
+    console.log('Basket 1 is: ', this.dataService.getBasketByName('Basket 1'));
+  }
+
+}
---
+++ src/app/data.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import baskets from './data/basket.json';
+import taxRules from './data/tax-rules.json';
+import { map, find } from 'lodash';
+
+export interface Basket {
+  name: string,
+  items: Item[];
+}
+
+export interface Item {
+  qty: number;
+  title: string;
+  price: number;
+  category: string; // need to make enum
+  imported?: boolean;
+}
+
+export interface TaxRule {
+  name: string;
+  rate: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+  getAllBaskets(): Basket[] {
+    return baskets;
+  }
+
+  getBasketNames(): string[] {
+    return map(baskets, basket => basket.name);
+  }
+
+  getBasketByName(name): Basket {
+    return find(baskets, basket => basket.name === name);
+  }
+
+  getTaxRules(): TaxRule[] {
+    return taxRules;
+  }
+}
